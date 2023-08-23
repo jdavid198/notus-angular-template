05ea77e6cad91748d6d9cd44c41ebd09a6057a61
@harvyM <harvyseptiembre@hotmail.com>
2023-08-22 23:59:37 -0500

    componente card-social-traffic

--- src/app/components/cards/card-social-traffic/card-social-traffic.component.ts
+++ src/app/components/cards/card-social-traffic/card-social-traffic.component.ts
@@ -1,11 +1,43 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+
+export type configTable={
+  header:string[] 
+  properties:string[]
+}
 
 @Component({
   selector: "app-card-social-traffic",
   templateUrl: "./card-social-traffic.component.html",
 })
 export class CardSocialTrafficComponent implements OnInit {
+  @Input("title")
+  protected title:string="Social traffic";
+  @Input("configTable")
+  protected config:configTable={
+    header:["Referral","Visitors"],
+    properties:["ref","vis"]
+  }
+  @Input("porcentageName")
+  protected portName:string="p";
+
+  @Input("data")
+  protected data:any[]=[
+    {ref:"Facebook",vis:"",p:60},
+    {ref:"Facebook",vis:"",p:40},
+    {ref:"Facebook",vis:"",p:50},
+    {ref:"Facebook",vis:"",p:30},
+    {ref:"Facebook",vis:"",p:20},
+    {ref:"Facebook",vis:"",p:90},
+  ];
+  @Input("type")
+  protected type:"danger" | "warning" | "success" | "info" | "barney" | "all" = "danger"
+
+  private allColorNumber=0;
+  protected getColor(){
+
+  }
   constructor() {}
 
   ngOnInit(): void {}
 }
+
